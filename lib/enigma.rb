e4a020fd0d1f75e5abd053cab77e7f9135f55711
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-05 11:08:11 -0600

    feat: add character set that enigma incodes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,12 @@
 require 'date'
 class Enigma
-  
+  attr_reader :characters
+
+  def initialize
+    @characters = ('a'..'z').to_a << ' '
+  end
+
   def incoming(incoming_phrase, incoming_key, incoming_date)
-    {phrase: incoming_phrase, key: incoming_key, date: incoming_date}
+    { phrase: incoming_phrase, key: incoming_key, date: incoming_date }
   end
 end
