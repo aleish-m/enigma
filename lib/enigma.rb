03a2fb524d658e4abc2358c0b8a485486832ad40
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-05 10:44:55 -0600

    test: add test to create incoming hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Enigma
   
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,3 +1,4 @@
+require_relative 'spec_helper'
 require './lib/enigma'
 
 describe Enigma do
@@ -5,9 +6,30 @@ describe Enigma do
     @enigma = Enigma.new
   end
 
-  describe 'Initalize' do
+  describe 'Initialize' do
     it 'Enigma exists' do
       expect(@enigma).to be_an_instance_of Enigma
+    end
+  end
+
+  describe 'encrypt' do
+    it 'creates an incoming hash' do
+      expected_hash = {
+        phrase: 'hello world',
+        key: '02715',
+        date: '040895'
+      }
+
+      expect(@enigma.incoming('hello world', '02715', '040895')).to eq expected_hash
+    end
+    xit 'encrypts text' do
+      expected_hash = {
+        encryption: 'keder ohulw',
+        key: '02715',
+        date: '040895'
+      }
+
+      expect(@enigma.encrypt('hello world', '02715', '040895')).to eq expected_hash
     end
   end
 end
