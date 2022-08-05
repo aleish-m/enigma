0b0e4a9d3382ccb6667f7c394ad8b86883fb04b8
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-05 11:41:52 -0600

    test: change test for how data to encrypt is received

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,7 +6,7 @@ class Enigma
     @characters = ('a'..'z').to_a << ' '
   end
 
-  def incoming(incoming_phrase, incoming_key, incoming_date)
-    { phrase: incoming_phrase, key: incoming_key, date: incoming_date }
-  end
+  # def incoming(incoming_phrase, incoming_key, incoming_date)
+  #   {phrase: incoming_phrase, key: incoming_key, date: incoming_date}
+  # end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -13,18 +13,42 @@ describe Enigma do
     it 'Enigma has a list of all incodeable characters' do
       expect(@enigma.characters).to eq ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     end
+
+    it 'has no incoming message, key or date when created when created' do
+      expect(@enigma.message).to eq ''
+      expect(@enigma.key).to eq ''
+      expect(@enigma.date).to eq ''
+    end
   end
 
   describe 'encrypt' do
-    it 'creates an incoming hash' do
-      expected_hash = {
-        phrase: 'hello world',
-        key: '02715',
-        date: '040895'
-      }
-
-      expect(@enigma.incoming('hello world', '02715', '040895')).to eq expected_hash
+    it 'adds message to be encrypted' do
+      @enigma.encrypt('hello world', '02715', '040895')
+      expect(@enigma.message).to eq 'hello world'
     end
+
+    it 'adds a key to use for encryption' do
+      @enigma.encrypt('hello world', '02715', '040895')
+
+      expect(@enigma.key).to eq '02715'
+    end
+
+    it 'adds a date to use for encryption' do
+      @enigma.encrypt('hello world', '02715', '040895')
+
+      expect(@enigma.date).to eq '040895'
+    end
+    
+    # it 'creates an incoming hash and stores it' do
+    #   expected_hash = {
+    #     phrase: 'hello world',
+    #     key: '02715',
+    #     date: '040895'
+    #   }
+
+    #   expect(@enigma.incoming('hello world', '02715', '040895')).to eq expected_hash
+    # end
+
     xit 'encrypts text' do
       expected_hash = {
         encryption: 'keder ohulw',
