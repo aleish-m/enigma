9064406117d6a36bb206d7e50a3aa3cdfbd08df5
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-05 19:19:34 -0600

    feat: create total shift amount

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,7 +9,6 @@ class Enigma
     @date = ''
   end
 
-
   def encrypt(incoming_phrase, incoming_key, incoming_date)
     @message = incoming_phrase
     @key = incoming_key
@@ -17,12 +16,15 @@ class Enigma
   end
 
   def key_shift
-    {A: @key[0..1].to_i, B: @key[1..2].to_i, C: @key[2..3].to_i, D: @key[3..4].to_i}
+    { A: @key[0..1].to_i, B: @key[1..2].to_i, C: @key[2..3].to_i, D: @key[3..4].to_i }
   end
 
   def date_shift
-    # require "pry"; binding.pry
-    date_code = ((@date.to_i**2).to_s)[-4..-1]
-    {A: date_code[0].to_i, B: date_code[1].to_i, C: date_code[2].to_i, D: date_code[3].to_i}
+    date_code = (@date.to_i**2).to_s[-4..-1]
+    { A: date_code[0].to_i, B: date_code[1].to_i, C: date_code[2].to_i, D: date_code[3].to_i }
+  end
+
+  def total_shift
+    key_shift.merge(date_shift) { |offset, key_value, date_value| key_value + date_value }
   end
 end
