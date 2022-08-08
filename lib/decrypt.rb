fb3639f602f719cfd36d55000787ca7db99b9c0e
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-07 18:45:19 -0600

    fix: correct command line error messages to better reflect program needs

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,8 +3,11 @@ enigma = Enigma.new
 
 first_file, second_file, *data = ARGV
 
-if ARGV.length < 3 || data.any?{|value| value.length > 6}
-  puts "We need at least 2 file names and the date in DDMMYY"
+if ARGV.length < 3
+  puts "We need at least 2 file names and the key."
+  exit
+elsif  data.any?{|value| value.length > 6}
+  "Please note: Date should be date in DDMMYY format."
   exit
 end
 
