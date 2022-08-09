96f1e5ac755490d050b34d8c152f784f422905f9
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-08 23:51:39 -0600

    fix forgot to remove a pry and to clean up encrypted.txt files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,8 +2,6 @@ require_relative './enigma'
 enigma = Enigma.new
 
 first_file, second_file, *data = ARGV
-require 'pry'
-binding.pry
 
 if ARGV.length < 3 || second_file[-4..-1] != '.txt'
   puts "We need at least 2 file names and the key. (Must write to '.txt' file)"
